03f88178ed81d8c847ffad0acbddbd28eeddd028
Joseph Chen <chenjose@usc.edu>
2016-04-18 20:53:19 -0700

    Further cleaning of old impl files

--- app/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask.ext.sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-from comment import Comment
-from vote import Vote
-from question import Question
-from post import Post
--- run.py
+++
@@ -1,3 +0,0 @@
-from app import create_app
-app = create_app()
-app.run(host="0.0.0.0", port=8000, debug=True)
